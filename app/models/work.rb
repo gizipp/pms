51a1bb1d851f3b878279aa348e2d9520df5c268d
Gizipp Aprianto <gizipp@softwareseni.com>
2014-09-11 17:27:48 +0700

    Add work user-work relation

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,6 @@
 class Work < ActiveRecord::Base
   belongs_to :users
+  has_many :contributors, :through => :appointments
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable, :timeoutable, :confirmable and :activatable
   devise :database_authenticatable, :registerable,
