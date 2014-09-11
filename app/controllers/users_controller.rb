de6ce88e13657cfb95e2a93ee8fc980334340caf
Gizipp Aprianto <gizipp@softwareseni.com>
2014-09-11 12:25:30 +0700

    Add validation on role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
-  validates :username, uniqueness: true
+  validates :username, presence: true
+  validates :username, uniqueness: true, if: -> { self.username.present? }
+  
+  #validate role (test)
+  #VALID_STATES = ["member", "admin"]
+  #SELECT_STATES = VALID_STATES.map { |s| [s.capitalize, s] }
+  #validates :role, :inclusion => { :in => Model::VALID_STATES }
+
+  #validate role
+  validates :role, inclusion: { in: %w(member admin), message: "%{value} is not a valid role" }
 end
