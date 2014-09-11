a103efcb663fdd9f220217faeda7f7c7aa14b41c
Gizipp Aprianto <gizipp@softwareseni.com>
2014-09-11 16:11:32 +0700

    Fix bug cannot added username to database when new user created or edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,19 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
-  attr_accessor :username
 
-  #validation
   validates :username, presence: true
   validates :username, uniqueness: true, if: -> { self.username.present? }
-
-  #validate role
   validates :role, inclusion: { in: %w(member admin), message: "%{value} is not a valid role" }
+
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
 end
