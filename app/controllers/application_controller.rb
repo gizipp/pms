8d828fc7a98da3f3d8d29ba1cae84ee5749e102d
Gizipp Aprianto <gizipp@softwareseni.com>
2014-09-11 11:15:32 +0700

    Fixing user login, register and edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_params, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  protected
+  def configure_permitted_params
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :username, :email, :password, :password_confirmation, :remember_me, :role) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :name, :username, :email, :role, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :username, :email, :password, :password_confirmation, :current_password, :role)}
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,3 @@
 class UsersController < ApplicationController
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  validates_uniqueness_of :username
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :username, :email, :role, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :username, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
-  end
+  validates :username, uniqueness: true
 end
