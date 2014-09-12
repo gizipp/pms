240ec439aaef649843fa5470b4eaf6864661161d
Gizipp Aprianto <gizipp@softwareseni.com>
2014-09-12 15:56:45 +0700

    Member can delete update their own works

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,21 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+
     if user.role? :admin
       can :manage, :all
     else
       can :read, :all
+      if user.role?(:member)
+        can :create, Work
+        can :destroy, Work do |work|
+             work.try(:user) == user
+        end
+        can :update, Work do |work|
+          work.try(:user) == user
+        end
+      end
     end
   end
 end
