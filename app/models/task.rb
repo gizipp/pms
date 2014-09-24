1875a60c72986aa04f356390906c59bf454c9598
Gizipp Aprianto <gizipp@softwareseni.com>
2014-09-24 08:41:42 +0700

    Add validation on due date. User can't set due date on the past.

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,13 @@ class Task < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :attachments, as: :attachable
+
+  validate :not_past_date
+
+  # Validate the the due date not in the past.
+  def not_past_date
+    if self.due_date < Date.today
+      errors.add(:due_date, 'not in past')
+    end
+  end
 end
